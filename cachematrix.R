19e792db57a6ae13994a9848cc7c8db5636b9fb0
zorex <johnzorex@gmail.com>
2014-08-24 00:01:25 +0200

    Added two functions + comments
    Filled in the body of makeCacheMatrix and cacheSolve and added comments
    about what the functions do

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function allows one to set the value of a matrix, get that value, set a matrix as the reverse of this matrix and get that reverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    r <- NULL
+    set <- function(y) {
+        x <<- y
+        r <<- NULL
+    }
+    get <- function() x
+    setreverse <- function(reversed) r <<- reversed
+    getreverse <- function() r
+    list(set = set, get = get,
+         setreverse = setreverse,
+         getreverse = getreverse)
 }
 
 
 ## Write a short comment describing this function
+## This function first tries to get the reverse of the matrix via the getreverse-function from makeCacheMatrix
+## if that works, it returns the reverse matrix, adding that it's being retrieved from cache
+## if it doesn't work (because the reverse matrix wasn't cached), it calculates the reverse using the solve-function and returns it
+## and makes sure the result gets cached via the setreverse-function from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        r <- x$getreverse()
+        if(!is.null(r)){
+            message("getting cached data")
+            return(r)
+        }
+        data <- x$get()
+        r <- solve(data, ...)
+        x$setreverse(r)
+        r
 }
